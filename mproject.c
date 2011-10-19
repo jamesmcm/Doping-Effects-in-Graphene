21cba3ddc49ddf4ae28e9374e597db2023496382
jamesmcm <jamesmcm03@hotmail.com>
2011-10-19 22:57:18 +0100

    broke everything

--- mproject.c
+++ mproject.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <Accelerate/Accelerate.h> //Works on OS X ONLY
+#include <complex.h>
 
-#define L 4 //colums
+//Complex double: zheev.f Computes all eigenvalues and, optionally, eigenvectors of a complex, Hermitian matrix.
+//
+
+#define L 3 //colums
 #define M 3 //rows
-#define WRAPX 1
-#define WRAPY 1
+#define T 1
+#define WRAPX 0
+#define WRAPY 0
+
 
 int main(){
-
+  int i;
   // create Hamiltonian
-  int H[L*M][L*M]={0}; //might want to use static here to ensure 0 setting in memory
+  double complex *Hcomplex = malloc(L*M*L*M*sizeof(double complex));
+  //Initialise array to zeroes
+  for(i=0;i<(L*M*L*M);i++){
+    *(Hcomplex+i)=0;
+  }
+  //*Hcomplex = 3.0 + (4.0*I);
+  //*(Hcomplex+1)= 2.0+(1.0*I);
 
+  //printf("%f + i%f\n", creal(*Hcomplex), cimag(*Hcomplex));
+  //printf("%f + i%f\n", creal(*(Hcomplex+1)), cimag(*(Hcomplex+1)));
+
+  //remember memory must be stored columns first, matrix must be hermitian
   int x=0;
   int y=0;
   int p=0;
@@ -19,40 +37,46 @@ int main(){
   /*   p++; */
   /* } */
 
+  //Hamiltonian must be Hermitian, so just generate up to diagnol and then flip
   while (y<M){
     while(x<L){
-      p=(x+(y*L)); //maybe use macro for this
-      if ((p+L)<(M*L)){
-	H[p][(p+L)]=1;
+      p=(x+(y*L)); //maybe use macro for this - p is in non-transposed space
+      ptrans=(y+(x*M));
+      if (((ptrans+L)/M) != (ptrans/M)){ //Node above in real space brick model is always connected, when transposed the Y conditions become the complicated ones!
+	//*(Hcomplex+(L*(p+L))+p)=T;
+	*(Hcomplex+ptrans+L; // doesn't work
+	//can use old conditions and swap for ptrans and M?q
       }
       else {
-	H[p][(p+L)-(M*L)]=WRAPY;
+	//*(Hcomplex+(L*((p+L)-(M*L)))+p)=WRAPY*T;
       }
       if ((p-L)>=0){
-	H[p][(p-L)]=1;
+	//*(Hcomplex+(L*(p-L))+p)=T;
       }
       else {
-	H[p][(p-L)+(M*L)]=WRAPY;
+	//*(Hcomplex+(L*((p-L)+(M*L))+p))=WRAPY*T;
+	
       }
-      //printf("p:%i -> %i\n", p, (p+((((p%L)%2)-!((p%L)%2))*((p/L)%2))+(((-1*((p%L)%2))+!((p%L)%2))*!((p/L)%2))));
-      if (((p+((((p%L)%2)-!((p%L)%2))*((p/L)%2))+(((-1*((p%L)%2))+!((p%L)%2))*!((p/L)%2)))/L) != (p/L)){
-	H[p][(((((p+((((p%L)%2)-!((p%L)%2))*((p/L)%2))+(((-1*((p%L)%2))+!((p%L)%2))*!((p/L)%2)))/L)-(p/L))<0)*((((p/L)+1)*L)-1))+(((((p+((((p%L)%2)-!((p%L)%2))*((p/L)%2))+(((-1*((p%L)%2))+!((p%L)%2))*!((p/L)%2)))/L)-(p/L))>0)*((((p/L))*L)))]=WRAPX;
+      if (((p+((((p%L)%2)-!((p%L)%2))*((p/L)%2)) +  (((-1*((p%L)%2))  +!((p%L)%2))*!((p/L)%2)))/L) != (p/L)){//connect xnode but this changes depending on position 
+	//*(Hcomplex+p+(L*((((((p+((((p%L)%2)-!((p%L)%2))*((p/L)%2))+(((-1*((p%L)%2))+!((p%L)%2))*!((p/L)%2)))/L)-(p/L))<0)*((((p/L)+1)*L)-1))+(((((p+((((p%L)%2)-!((p%L)%2))*((p/L)%2))+(((-1*((p%L)%2))+!((p%L)%2))*!((p/L)%2)))/L)-(p/L))>0)*((((p/L))*L))))))=WRAPX*T;
       }
       else {
-	H[p][(p+((((p%L)%2)-!((p%L)%2))*((p/L)%2))+(((-1*((p%L)%2))+!((p%L)%2))*!((p/L)%2)))]=1;
+	//*(Hcomplex+p+(L*((p+((((p%L)%2)-!((p%L)%2))*((p/L)%2))+(((-1*((p%L)%2))+!((p%L)%2))*!((p/L)%2))))))=T;
       }
       x++;
     }
     y++;
-    x=0;
+    x=y;
   }
 
+  //FLIP MATRIX
+
   //Print Hamiltonian
-  int i=0;
+  i=0;
   int j=0;
   while (j<(L*M)){
     while (i<(L*M)){
-      printf("%i ", H[j][i]);
+      printf("%.0f ", creal(*(Hcomplex+j+(L*i))));
       i++;
     }
     i=0;
