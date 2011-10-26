c1352779491e7560b3884771d76bfa24a088faa4
jamesmcm <jamesmcm03@hotmail.com>
2011-10-26 15:28:12 +0100

    GSL gets eigenvalues, segfaults at large L,M though

--- mprojectgsl.c
+++ mprojectgsl.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <gsl/gsl_math.h>
 #include <gsl/gsl_eigen.h>
 //#include <Accelerate/Accelerate.h> //Works on OS X ONLY
@@ -6,16 +7,16 @@
 //Complex double: zheev.f Computes all eigenvalues and, optionally, eigenvectors of a complex, Hermitian matrix.
 //
 
-#define L 4 //colums
-#define M 4 //rows
+#define L 11 //colums
+#define M 11 //rows
 #define WRAPX 1
 #define WRAPY 0
 
 int main(){
 
   // create Hamiltonian
-  double H[L*M][L*M]={0}; //might want to use static here to ensure 0 setting in memory
-
+  double H[L*M][L*M]={0}; //might want to use static here to ensure 0 setting in memory - this segfaults for large numbers of L,M
+  //memset(H, 0, sizeof(double));
   int x=0;
   int y=0;
   int p=0;
@@ -29,23 +30,23 @@ int main(){
     while(x<L){
       p=(x+(y*L)); //maybe use macro for this
       if ((p+L)<(M*L)){
-	H[p][(p+L)]=1;
+  	H[p][(p+L)]=1;
       }
       else {
-	H[p][(p+L)-(M*L)]=WRAPY;
+  	H[p][(p+L)-(M*L)]=WRAPY;
       }
       if ((p-L)>=0){
-	H[p][(p-L)]=1;
+  	H[p][(p-L)]=1;
       }
       else {
-	H[p][(p-L)+(M*L)]=WRAPY;
+  	H[p][(p-L)+(M*L)]=WRAPY;
       }
       //printf("p:%i -> %i\n", p, (p+((((p%L)%2)-!((p%L)%2))*((p/L)%2))+(((-1*((p%L)%2))+!((p%L)%2))*!((p/L)%2))));
       if (((p+((((p%L)%2)-!((p%L)%2))*((p/L)%2))+(((-1*((p%L)%2))+!((p%L)%2))*!((p/L)%2)))/L) != (p/L)){
-	H[p][(((((p+((((p%L)%2)-!((p%L)%2))*((p/L)%2))+(((-1*((p%L)%2))+!((p%L)%2))*!((p/L)%2)))/L)-(p/L))<0)*((((p/L)+1)*L)-1))+(((((p+((((p%L)%2)-!((p%L)%2))*((p/L)%2))+(((-1*((p%L)%2))+!((p%L)%2))*!((p/L)%2)))/L)-(p/L))>0)*((((p/L))*L)))]=WRAPX;
+  	H[p][(((((p+((((p%L)%2)-!((p%L)%2))*((p/L)%2))+(((-1*((p%L)%2))+!((p%L)%2))*!((p/L)%2)))/L)-(p/L))<0)*((((p/L)+1)*L)-1))+(((((p+((((p%L)%2)-!((p%L)%2))*((p/L)%2))+(((-1*((p%L)%2))+!((p%L)%2))*!((p/L)%2)))/L)-(p/L))>0)*((((p/L))*L)))]=WRAPX;
       }
       else {
-	H[p][(p+((((p%L)%2)-!((p%L)%2))*((p/L)%2))+(((-1*((p%L)%2))+!((p%L)%2))*!((p/L)%2)))]=1;
+  	H[p][(p+((((p%L)%2)-!((p%L)%2))*((p/L)%2))+(((-1*((p%L)%2))+!((p%L)%2))*!((p/L)%2)))]=1;
       }
       x++;
     }
@@ -77,13 +78,13 @@ int main(){
      
   gsl_eigen_symmv_free (w);
      
-  gsl_eigen_symmv_sort (eval, evec, GSL_EIGEN_SORT_ABS_ASC);
-
+  //gsl_eigen_symmv_sort (eval, evec, GSL_EIGEN_SORT_ABS_ASC);
+  int i=0;
   for (i = 0; i < L*M; i++)
     {
       double eval_i = gsl_vector_get (eval, i);
-      gsl_vector_view evec_i = gsl_matrix_column (evec, i);
-      printf ("eigenvalue = %g\n", eval_i);
+      //gsl_vector_view evec_i = gsl_matrix_column (evec, i);
+      printf ("%g\n", eval_i); //eigenvalue
       //printf ("eigenvector = \n");
       //gsl_vector_fprintf (stdout, &evec_i.vector, "%g");
            }
