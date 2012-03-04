451e27763395e5d2af6011dc59a106611eb4d249
Chris <cbeckerleg101@gmail.com>
2012-03-04 17:51:39 +0000

    Octave script

---
+++ Compare/comp.m
@@ -0,0 +1,340 @@
+load code_1.txt
+load ana_1.txt 
+
+x=code_1(:,1);
+y=code_1(:,2);
+z=code_1(:,3);
+
+a=ana_1(:,1);
+b=ana_1(:,2);
+c=ana_1(:,3);
+      
+
+p = y-c;
+q = z-b;
+
+mn_p=mean(p)
+va_p=std(p)
+
+mn_q=mean(q)
+va_q=std(q)
+
+hold on
+plot(x,p,'go')
+plot(x,q,'mo')
+hold off
+print("Comp_1.png")
+
+clear
+
+load code_6.txt
+load ana_6.txt 
+
+x=code_6(:,1);
+y=code_6(:,2);
+z=code_6(:,3);
+
+a=ana_6(:,1);
+b=ana_6(:,2);
+c=ana_6(:,3);
+      
+
+p = y-c;
+q = z-b;
+
+mn_p=mean(p)
+va_p=std(p)
+
+mn_q=mean(q)
+va_q=std(q)
+
+hold on
+plot(x,p,'go')
+plot(x,q,'mo')
+hold off
+print("Comp_6.png")
+
+clear
+
+load code_11.txt
+load ana_11.txt 
+
+x=code_11(:,1);
+y=code_11(:,2);
+z=code_11(:,3);
+
+a=ana_11(:,1);
+b=ana_11(:,2);
+c=ana_11(:,3);
+      
+
+p = y-c;
+q = z-b;
+
+mn_p=mean(p)
+va_p=std(p)
+
+mn_q=mean(q)
+va_q=std(q)
+
+hold on
+plot(x,p,'go')
+plot(x,q,'mo')
+hold off
+print("Comp_11.png")
+
+clear
+load code_16.txt
+load ana_16.txt 
+
+x=code_16(:,1);
+y=code_16(:,2);
+z=code_16(:,3);
+
+a=ana_16(:,1);
+b=ana_16(:,2);
+c=ana_16(:,3);
+      
+
+p = y-c;
+q = z-b;
+
+mn_p=mean(p)
+va_p=std(p)
+
+mn_q=mean(q)
+va_q=std(q)
+
+hold on
+plot(x,p,'go')
+plot(x,q,'mo')
+hold off
+print("Comp_16.png")
+
+clear
+load code_16.txt
+load ana_16.txt 
+
+x=code_16(:,1);
+y=code_16(:,2);
+z=code_16(:,3);
+
+a=ana_16(:,1);
+b=ana_16(:,2);
+c=ana_16(:,3);
+      
+
+p = y-c;
+q = z-b;
+
+mn_p=mean(p)
+va_p=std(p)
+
+mn_q=mean(q)
+va_q=std(q)
+
+hold on
+plot(x,p,'go')
+plot(x,q,'mo')
+hold off
+print("Comp_16.png")
+
+clear
+load code_21.txt
+load ana_21.txt 
+
+x=code_21(:,1);
+y=code_21(:,2);
+z=code_21(:,3);
+
+a=ana_21(:,1);
+b=ana_21(:,2);
+c=ana_21(:,3);
+      
+
+p = y-c;
+q = z-b;
+
+mn_p=mean(p)
+va_p=std(p)
+
+mn_q=mean(q)
+va_q=std(q)
+
+hold on
+plot(x,p,'go')
+plot(x,q,'mo')
+hold off
+print("Comp_21.png")
+
+clear
+load code_26.txt
+load ana_26.txt 
+
+x=code_26(:,1);
+y=code_26(:,2);
+z=code_26(:,3);
+
+a=ana_26(:,1);
+b=ana_26(:,2);
+c=ana_26(:,3);
+      
+
+p = y-c;
+q = z-b;
+
+mn_p=mean(p)
+va_p=std(p)
+
+mn_q=mean(q)
+va_q=std(q)
+
+hold on
+plot(x,p,'go')
+plot(x,q,'mo')
+hold off
+print("Comp_26.png")
+
+clear
+load code_30.txt
+load ana_30.txt 
+
+x=code_30(:,1);
+y=code_30(:,2);
+z=code_30(:,3);
+
+a=ana_30(:,1);
+b=ana_30(:,2);
+c=ana_30(:,3);
+      
+
+p = y-c;
+q = z-b;
+
+mn_p=mean(p)
+va_p=std(p)
+
+mn_q=mean(q)
+va_q=std(q)
+
+hold on
+plot(x,p,'go')
+plot(x,q,'mo')
+hold off
+print("Comp_30.png")
+
+clear
+
+load code_50.txt
+load ana_50.txt 
+
+x=code_50(:,1);
+y=code_50(:,2);
+z=code_50(:,3);
+
+a=ana_50(:,1);
+b=ana_50(:,2);
+c=ana_50(:,3);
+      
+
+p = y-c;
+q = z-b;
+
+mn_p=mean(p)
+va_p=std(p)
+
+mn_q=mean(q)
+va_q=std(q)
+
+hold on
+plot(x,p,'go')
+plot(x,q,'mo')
+hold off
+print("Comp_50.png")
+
+clear
+
+load code_70.txt
+load ana_70.txt 
+
+x=code_70(:,1);
+y=code_70(:,2);
+z=code_70(:,3);
+
+a=ana_70(:,1);
+b=ana_70(:,2);
+c=ana_70(:,3);
+      
+
+p = y-c;
+q = z-b;
+
+mn_p=mean(p)
+va_p=std(p)
+
+mn_q=mean(q)
+va_q=std(q)
+
+hold on
+plot(x,p,'go')
+plot(x,q,'mo')
+hold off
+print("Comp_70.png")
+
+clear
+load code_90.txt
+load ana_90.txt 
+
+x=code_90(:,1);
+y=code_90(:,2);
+z=code_90(:,3);
+
+a=ana_90(:,1);
+b=ana_90(:,2);
+c=ana_90(:,3);
+      
+
+p = y-c;
+q = z-b;
+
+mn_p=mean(p)
+va_p=std(p)
+
+mn_q=mean(q)
+va_q=std(q)
+
+hold on
+plot(x,p,'go')
+plot(x,q,'mo')
+hold off
+print("Comp_90.png")
+
+clear
+load code_110.txt
+load ana_110.txt 
+
+x=code_110(:,1);
+y=code_110(:,2);
+z=code_110(:,3);
+
+a=ana_110(:,1);
+b=ana_110(:,2);
+c=ana_110(:,3);
+      
+
+p = y-c;
+q = z-b;
+
+mn_p=mean(p)
+va_p=std(p)
+
+mn_q=mean(q)
+va_q=std(q)
+
+hold on
+plot(x,p,'go')
+plot(x,q,'mo')
+hold off
+print("Comp_110.png")
+
+clear
