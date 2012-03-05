c83a91af494412c952014b50b1d4e95d27d6d191
Chris <cbeckerleg101@gmail.com>
2012-03-05 13:10:47 +0000

    Updated C code

--- analytical.c
+++ analytical.c
@@ -3,9 +3,14 @@
 #include <stdlib.h>
 #include <complex.h>
 
+#define SIZE 2   //Range over which epsilon varies
+
+//#define N 7    //Program is specific to a given N value
 
 double comp(double lam,int n);
 double real(double lam,int n);
+double f_top(double t,double t_neg);
+double f_down(double t,double t_neg);
 
 int main (){
 
@@ -15,14 +20,17 @@ double eps,lam,lam_neg;
 double a_n,b_n,c_n,d_n;
 double a_neg,b_neg,c_neg,d_neg;
 double t,t_neg;
+double top,down;
 
 int n=0;
 eps=0;
 lam=0;
 lam_neg=0;
-      scanf("%d",&n);  //Program is specific to a given N value - reads in from command line
+top=0;
+down=0;
+      scanf("%d",&n);
   for(i=0;i<100000;i++){
-	eps=((i/10000)-5.001); //Forces range over -5<x<5
+	eps=((i/10000)-5.00);
 	lam = (eps+1);
 	lam_neg= (eps-1);	
 
@@ -31,28 +39,40 @@ lam_neg=0;
 		if((fabs(lam)<2)&&(fabs(lam_neg)<2)){  
     		t= comp(lam,n);
 		t_neg=comp(lam_neg,n);
-		printf("%f %f %f\n",eps,t,t_neg);
+
+		//printf("%e %e %e\n",eps,t,t_neg);
 
 		}
 		if((fabs(lam)>2)&&(fabs(lam_neg)<2)){
 
 		 t=real(lam,n);
 		 t_neg=comp(lam_neg,n);
-		printf("%f %f %f\n",eps,t,t_neg);
+                
+		//printf("%e %e %e\n",eps,t,t_neg);
 		}
 
 		if((fabs(lam)<2)&&(fabs(lam_neg)>2)){
 		 t=comp(lam,n);  
     		 t_neg= real(lam_neg,n);
-		 printf("%f %f %f\n",eps,t,t_neg);
+           
+		 //printf("%e %e %e\n",eps,t,t_neg);
 
 		}
 		if((fabs(lam_neg)>2)&&(fabs(lam)>2)){
 		 t=real(lam,n);
 		 t_neg=real(lam_neg,n);
-		printf("%f %f %f\n",eps,t,t_neg);
+            
+		//printf("%e %e %e\n",eps,t,t_neg);
 		}
+      
+     
+      down  = f_down(t,t_neg);
+      top     = f_top(t,t_neg);
 
+
+     
+
+      printf("%e %e %e\n",eps,down,top);
    }
 		
 
@@ -60,7 +80,7 @@ lam_neg=0;
 return 0;
 
 }
-
+//Function for complex lambda
 double comp(double lam,int n){
 
 double a_n=0,b_n=0,c_n=0,d_n=0,t=0,t_neg=0,arg1,arg;
@@ -81,7 +101,7 @@ arg = (atan2((sqrt(1-pow(lam,2)/4)),(lam/2)));
 return (t);
 
 }
-
+//Function for real lambda
 double real(double lam,int n){
 double a_n=0,b_n=0,c_n=0,d_n=0,t=0,xi=0,cons;
 
@@ -101,11 +121,21 @@ cons= (1/(sqrt( pow(lam,2)-4)));
 return (t);
 }
 
+double f_top(double t,double t_neg){
+ t   = (t>t_neg)? t:t_neg;
 
 
+return (t);
+
+}
+
+double f_down(double t,double t_neg){
+
+ t_neg =(t_neg<t)? t_neg:t;
 
 
-
+return(t_neg);
+}     
 
 
 
