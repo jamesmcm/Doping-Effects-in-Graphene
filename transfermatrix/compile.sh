5c3ff0a1850424cc1ea513b4f93d49de9330e964
jamesmcm <jamesmcm03@hotmail.com>
2012-03-09 21:51:44 +0000

    Fixed warnings, etc. - problem still remains

--- transfermatrix/compile.sh
+++ transfermatrix/compile.sh
@@ -1,1 +1,1 @@
-gfortran driver.f lingalg.f tmatrix.f trupdate.f util.f -o bin.out -L/usr/local/lib -llapack -lblas
+gfortran driver.f linalg.f tmatrix.f trupdate.f util.f -o bin.out -L/usr/local/lib -llapack -lblas
