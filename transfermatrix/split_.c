b907058e77cbc7b46a72163b2ab578bb86d8741d
Andrey V. Shytov <avs@quad.wired.home>
2012-03-19 16:56:09 +0000

    Comments in split_.c

--- transfermatrix/split_.c
+++ transfermatrix/split_.c
@@ -3,32 +3,38 @@
 
 void split_ (const int *plimx, const double *mult, 
 	     double *a, double *b, double *c, double *d) {
+      // HACK! The arrays are in fact complex-valued, 
+      // but here I interpret them as double arrays, 
+      // by doubling their length
       int i; 
+  
+      int limx = *plimx;  
+      int da   = 2 * limx; // 2 because of sizeof(complex) 
+      int dm   = 2 * da;   // two halves of a col belong to
+                           // two different matrices 
+      int szcp = da * sizeof (double); // Chunk to copy
    
-      int limx = *plimx; 
-      int da   = 2 * limx; 
-      int dm   = 2 * da; 
-      int szcp = da * sizeof (double);
-   
-      double *pmult = mult; 
+      double *pmult = mult;  // Set initial pointers
       double *pa = a; 
       double *pb = b;
       double *pc = c; 
       double *pd = d;
      
+      // First limx cols of MULT are to be split into 
+      // cols of A and B
+      // First half goes into A, second half into B, 
+      // and we increment the pointers accordingly.
+      // We rely on Fortran array ordering convention:
+      // A(1, 1), A(2, 1), A(3, 1), ... A(N, 1), A(1, 2), A(2, 2), etc
       for (i = 0; i < limx; i ++, pa += da, pb += da, pmult += dm) {
-	   //double *pmult = mult + 4 * limx * i; 
-	   //double *pa = a + 2 * limx * i; 
-	   //double *pb = b + 2 * limx * i; 
            memcpy (pa, pmult,      szcp); 
            memcpy (pb, pmult + da, szcp); 
       }
    
+      // The remaining cols of MULT are to be split into 
+      // cols of C and D
       pmult = mult + 2 * limx * da; 
       for (i = 0; i < limx; i ++, pc += da, pd += da, pmult += dm) {
-//	   double *pmult = mult + 4 * limx * i + 4 * limx * limx; 
-//	   double *pc = c + 2 * limx * i; 
-//	   double *pd = d + 2 * limx * i; 
            memcpy (pc, pmult,      szcp); 
            memcpy (pd, pmult + da, szcp); 
       }
