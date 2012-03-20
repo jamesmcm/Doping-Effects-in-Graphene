cf59099c9ab8fd9a444cd7b4445fa6059240ef89
Andrey V. Shytov <avs@quad.wired.home>
2012-03-20 19:22:10 +0000

    New functionality: hofstadter.py can now be used for testing
    Added the capability to store the data, and compare the new data
    to the one stored before. Can be used to test the code systematically.

--- transfermatrix/hofstadter.py
+++ transfermatrix/hofstadter.py
@@ -6,6 +6,60 @@ import math
 Lx = 4
 Ly = 7
 
+def SaveData (Xy, Yu, Zu, Xw, Yw, Zw):
+    import shelve
+    f = shelve.open('hofstadter-%dx%d.dat' % (Lx, Ly))
+    f['Xw'] = Xw
+    f['Yw'] = Yw
+    f['Zw'] = Zw
+    f['Xu'] = Xu
+    f['Yu'] = Yu
+    f['Zu'] = Zu
+    f.close()
+    
+
+def CompareOldData (Xu, Yu, Zu, Xw, Yw, Zw):
+    try: 
+        import shelve
+        f = shelve.open('hofstadter-%dx%d.dat' % (Lx, Ly))
+        Xwold = f['Xw']
+        Ywold = f['Yw']
+        Zwold = f['Zw']
+        Xuold = f['Xu']
+        Yuold = f['Yu']
+        Zuold = f['Zu']
+        f.close()
+        dxu = norm (Xu - Xuold)
+        dyu = norm (Yu - Yuold)
+        dzu = norm (Zu - Zuold)
+        print "dxu: ", dxu, "dyu:", dyu, "dzw", dzu
+        if (dxu >  1e-6) or (dyu > 1e-6):
+            print "Grid has changed, comparison is meaningless"
+            return None
+    
+        figure() 
+        title ('Diff for unwrapped case')
+        pcolor (Xu, Yu, Zu - Zuold)
+        colorbar()
+    
+        dxw = norm (Xw - Xwold)
+        dyw = norm (Yw - Ywold)
+        dzw = norm (Zw - Zwold)
+        print "dxw: ", dxw, "dyw:", dyw, "dzw:", dzw
+        if (dxw >  1e-6) or (dyw > 1e-6):
+            print "Grid has changed, comparison is meaningless"
+            return None
+        figure() 
+        title ('Diff for wrapped case')
+        pcolor (Xw, Yw, Zw - Zwold)
+        colorbar()
+        return dzu + dzw
+    except:
+        pass
+    print "Cannot read saved data to do comparison"
+    return None
+    
+    
 def G (E, Phi, Wrap):
     tl, c = tmatrix.gettrans (Lx, Ly, E, Phi, Wrap)
     tvals = array(tl)
@@ -20,16 +74,16 @@ def G (E, Phi, Wrap):
 
 figure()
 Evals = arange (-4.0, 4.01, 0.01)
-Phivals = arange (0.0, 2.0*math.pi + 0.0001, 2.0*math.pi/400); 
+Phivals = arange (0.0, 2.0*math.pi + 0.0001, 2.0*math.pi/100); 
 
-X, Y = meshgrid (Phivals, Evals)
-Z = zeros (shape(X))
+Xu, Yu = meshgrid (Phivals, Evals)
+Zu = zeros (shape(Xu))
 for i in range (len (Evals)):
-    print "NOWRAP: E = ", Y[i, 0]
+    print "NOWRAP: E = ", Yu[i, 0]
     for j in range(len (Phivals)):
-        Z[i, j] = G(Y[i, j], X[i, j], False)
+        Zu[i, j] = G(Yu[i, j], Xu[i, j], False)
         
-pcolor (X/2.0/math.pi, Y, Z)
+pcolor (Xu/2.0/math.pi, Yu, Zu)
 xlim(0.0, 1.0)
 ylim(-4.0, 4.0)
 title ('Open boundary')
@@ -39,16 +93,16 @@ colorbar()
 
 figure()
 Evals = arange (-4.0, 4.01, 0.01)
-Phivals = arange (0.0, 2.0*math.pi + 0.0001, 2.0*math.pi/400); 
+Phivals = arange (0.0, 2.0*math.pi + 0.0001, 2.0*math.pi/100); 
 
-X, Y = meshgrid (Phivals, Evals)
-Z = zeros (shape(X))
+Xw, Yw = meshgrid (Phivals, Evals)
+Zw = zeros (shape(Xw))
 for i in range (len (Evals)):
-    print "WRAP: E = ", Y[i, 0]
+    print "WRAP: E = ", Yw[i, 0]
     for j in range(len (Phivals)):
-        Z[i, j] = G(Y[i, j], X[i, j], True)
+        Zw[i, j] = G(Yw[i, j], Xw[i, j], True)
         
-pcolor (X/2.0/math.pi, Y, Z)
+pcolor (Xw/2.0/math.pi, Yw, Zw)
 xlim(0.0, 1.0)
 ylim(-4.0, 4.0)
 title ('Wrapped boundary')
@@ -56,6 +110,10 @@ xlabel (r'Magnetic flux $\Phi / \Phi_0$')
 ylabel (r'Energy $\epsilon/t$')
 colorbar()
 
+if CompareOldData (Xu, Yu, Zu, Xw, Yw, Zw) == None: 
+    print "Saving the data"
+    SaveData (Xu, Yu, Zu, Xw, Yw, Zw)
+    
 show ()
         
     
