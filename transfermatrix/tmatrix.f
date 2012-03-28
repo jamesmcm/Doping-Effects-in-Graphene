ebc7a08d6cba2b583a640bf4a80235c032ec28fe
jamesmcm <jamesmcm03@hotmail.com>
2012-03-28 13:12:03 +0100

    Modified CALCMULTXY to be like new form of CALCMULT. Began the Great Renaming, and adding YY functions from tmatrix2.f. Still need to add merge new GETTRANS functions, and modify driver.f to allow a choice between these. Added new GETTRANSXY for X function, however this is broken at the moment, I assume the form of U is wrong and must be modified somehow.

--- transfermatrix/tmatrix.f
+++ transfermatrix/tmatrix.f
@@ -72,7 +72,7 @@ c$$$  Originally the first M matrix was set here
       RETURN
       END
 
-      SUBROUTINE CALCMULTNEW(LIMX, WRAPX, MULT, E, FLUX, POS)
+      SUBROUTINE CALCMULTYX(LIMX, WRAPX, MULT, E, FLUX, POS)
       IMPLICIT NONE
       INTEGER LIMX, WRAPX
       DOUBLE COMPLEX MULT(2*LIMX, 2*LIMX)
@@ -164,15 +164,15 @@ C$$$     AGAIN, THE CODE IS NOW RATHER UGLY.
       DOUBLE COMPLEX U(LIMX,LIMX)
 
 c      CALL CALCMULT(LIMX, WRAPX, MODD, MEVEN, E, FLUX)
-      CALL CALCMULTNEW(LIMX, WRAPX, MODD, E, FLUX, 1)
-      CALL CALCMULTNEW(LIMX, WRAPX, MEVEN, E, FLUX, 2)
+      CALL CALCMULTYX(LIMX, WRAPX, MODD, E, FLUX, 1)
+      CALL CALCMULTYX(LIMX, WRAPX, MEVEN, E, FLUX, 2)
 c$$$  CALCMULT fills MODD, MEVEN - do multiplication in main loop
 c$$$  Must decide whether we want zig-zag or armchair edges
 C     For now I have left it as before so I can compare results
 
       CALL SQUNIT (MULT, 2*LIMX)
 
-      CALL FILLU(U, LIMX, FLUX)
+      CALL FILLUYX(U, LIMX, FLUX)
 
       CALL SQUNIT (T, LIMX)
       CALL SQUNIT (TTILDE, LIMX)
@@ -201,8 +201,69 @@ c$$$  CheckUni2 is slightly faster --- AVS
       RETURN
       END
 
+      DOUBLE PRECISION FUNCTION GETTRANSXY(TVALS, LIMX, NSIZE,
+     +   E, FLUX)
+      IMPLICIT NONE
+C     NSIZE = LIMY/2
+      INTEGER I/1/, NSIZE, LIMX
+      DOUBLE PRECISION TVALS(NSIZE), E, FLUX
+      DOUBLE PRECISION CHECKUNI
+      EXTERNAL CHECKUNI
+      DOUBLE PRECISION CHECKUNI2
+      EXTERNAL CHECKUNI2
+      DOUBLE COMPLEX MODD(2*NSIZE, 2*NSIZE), MEVEN(2*NSIZE, 2*NSIZE),
+     +               MULT(2*NSIZE, 2*NSIZE)
+      DOUBLE COMPLEX A(NSIZE, NSIZE), B(NSIZE, NSIZE),
+     +               C(NSIZE, NSIZE), D(NSIZE, NSIZE)
+      DOUBLE COMPLEX T(NSIZE, NSIZE),    TTILDE(NSIZE, NSIZE),
+     +               R(NSIZE, NSIZE),    RTILDE(NSIZE, NSIZE),
+     +               TINC(NSIZE, NSIZE), TTILDEINC(NSIZE, NSIZE),
+     +               RINC(NSIZE, NSIZE), RTILDEINC(NSIZE, NSIZE)
+      DOUBLE COMPLEX U(NSIZE,NSIZE)
+
+c      CALL CALCMULT(LIMX, WRAPX, MODD, MEVEN, E, FLUX)
+c$$$      CALL CALCMULTNEW(LIMX, WRAPX, MODD, E, FLUX, 1)
+c$$$      CALL CALCMULTNEW(LIMX, WRAPX, MEVEN, E, FLUX, 2)
+      CALL CALCMULTXY(NSIZE, MODD, E, FLUX, 1)
+      CALL CALCMULTXY(NSIZE, MEVEN, E, FLUX, 2)
+
+c$$$  CALCMULT fills MODD, MEVEN - do multiplication in main loop
+c$$$  Must decide whether we want zig-zag or armchair edges
+C     For now I have left it as before so I can compare results
+
+      CALL SQUNIT (MULT, 2*NSIZE)
+
+      CALL FILLUYX(U, NSIZE, FLUX)
+
+      CALL SQUNIT (T, NSIZE)
+      CALL SQUNIT (TTILDE, NSIZE)
+      CALL SQZERO (R, NSIZE)
+      CALL SQZERO (RTILDE, NSIZE)
+
+      DO I = 1, LIMX
+            IF (MOD(I,2) .EQ. 1) THEN
+               CALL SQCOPY (MODD, MULT, 2*NSIZE) 
+            ELSE
+               CALL SQCOPY(MEVEN, MULT, 2*NSIZE)
+            END IF
+
+        
+            CALL GENABCD(NSIZE,MULT,A,B,C,D,U)
+            CALL GENTANDRINC(NSIZE, TINC, RINC, TTILDEINC, RTILDEINC,
+     +           A, B, C,D)
+            CALL UPDATETANDR(T,     R,    TTILDE,    RTILDE, 
+     +                       TINC,  RINC, TTILDEINC, RTILDEINC, 
+     +                       NSIZE)
+      END DO
+      CALL SV_DECOMP(NSIZE, T, TVALS)
+
+c$$$  CheckUni2 is slightly faster --- AVS
+      GETTRANSXY = CHECKUNI2(NSIZE,T,R,TTILDE,RTILDE)
+      RETURN
+      END
+
 c$$$  AIM TO CHANGE TO FILL(U,LIMX,FLUX)
-      SUBROUTINE FILLU(U, LIMX, FLUX)
+      SUBROUTINE FILLUYX(U, LIMX, FLUX)
       IMPLICIT NONE
       INTEGER LIMX, I
       DOUBLE COMPLEX U(LIMX,LIMX)
@@ -219,58 +280,62 @@ c$$$  AIM TO CHANGE TO FILL(U,LIMX,FLUX)
       RETURN
       END
 
-      SUBROUTINE CALCMULTXY(NSIZE, MODD, MEVEN, E, FLUX)
-C     NOTE LIMY MUST BE PASSED AS CONSTANT PARAMETER FOR THIS
+      SUBROUTINE CALCMULTXY(NSIZE, MULT, E, FLUX, POS)
+C     NOTE LIMY MUST BE PASSED AS CONSTANT PARAMETER FOR THIS, LIMY MUST BE EVEN
       IMPLICIT NONE
-      INTEGER LIMX
+      INTEGER LIMX, POS
 C     WRAPY ignored
       INTEGER I/1/, NSIZE
       DOUBLE PRECISION E, FLUX
       DOUBLE COMPLEX CNUM
 
-      DOUBLE COMPLEX MODD(NSIZE*2, NSIZE*2), MEVEN(NSIZE*2, NSIZE*2), 
-     +     N3EVEN(NSIZE, NSIZE), N2EVEN(NSIZE, NSIZE), 
-     +     N3ODD(NSIZE, NSIZE), N2ODD(NSIZE, NSIZE)
+      DOUBLE COMPLEX MULT(NSIZE*2, NSIZE*2), 
+     +     N3(NSIZE, NSIZE), N2(NSIZE, NSIZE)
 
 C     Odd is defined for odd leftmost column, even for even leftmost column
-      CALL SQZERO (N3ODD, NSIZE)
-      CALL SQZERO (N2ODD, NSIZE)
-      CALL SQZERO (N3EVEN, NSIZE)
-      CALL SQZERO (N2EVEN, NSIZE)
+      CALL SQZERO (N3, NSIZE)
+      CALL SQZERO (N2, NSIZE)
 
       DO I = 1, NSIZE
-         N3ODD(I, I)=1
-         N2ODD(I, I)=1
-         N3EVEN(I, I)=1
-         N2EVEN(I, I)=1
+         N3(I, I)=1
+         N2(I, I)=1
          IF (I .NE. 1) THEN
-            N3ODD(I,I-1)=1
-            N2EVEN(I,I-1)=1
+            IF (MOD(POS,2) .EQ. 0) THEN
+               N2(I,I-1)=1
+            ELSE
+               N3(I,I-1)=1
+            ENDIF
          ENDIF
          IF (I .NE. NSIZE) THEN
-            N2ODD(I,I+1)=1
-            N3EVEN(I,I+1)=1
+            IF (MOD(POS,2) .EQ. 0) THEN
+               N3(I,I+1)=1
+            ELSE
+               N2(I,I+1)=1
+            ENDIF
          ENDIF
       ENDDO
 
 C     M={{-N3^-1, E*N3^-1},{-E*N3^-1, E^2 N3^-1 - N2^-1}}
 
-      CALL INVERTMATRIX(N3ODD, NSIZE)
-      CALL INVERTMATRIX(N2ODD, NSIZE)
+      CALL INVERTMATRIX(N3, NSIZE)
+      CALL INVERTMATRIX(N2, NSIZE)
 
-      MODD(1:NSIZE, 1:NSIZE)=-1*N3ODD
-      MODD(1:NSIZE, NSIZE+1:2*NSIZE)=E*N3ODD
-      MODD(NSIZE+1:2*NSIZE, 1:NSIZE)=-E*N3ODD
-      MODD(NSIZE+1:2*NSIZE, NSIZE+1:2*NSIZE)=(E*E*N3ODD) - N2ODD
+      MULT(1:NSIZE, 1:NSIZE)=-1*N3
+      MULT(1:NSIZE, NSIZE+1:2*NSIZE)=E*N3
+      MULT(NSIZE+1:2*NSIZE, 1:NSIZE)=-E*N3
+      MULT(NSIZE+1:2*NSIZE, NSIZE+1:2*NSIZE)=(E*E*N3) - N2
 
-      CALL INVERTMATRIX(N3EVEN, NSIZE)
-      CALL INVERTMATRIX(N2EVEN, NSIZE)
+      RETURN
+      END
 
-      MEVEN(1:NSIZE, 1:NSIZE)=-1*N3EVEN
-      MEVEN(1:NSIZE, NSIZE+1:2*NSIZE)=E*N3EVEN
-      MEVEN(NSIZE+1:2*NSIZE, 1:NSIZE)=-E*N3EVEN
-      MEVEN(NSIZE+1:2*NSIZE, NSIZE+1:2*NSIZE)=(E*E*N3EVEN) - N2EVEN
-c$$$      CALL ZPRINTM(MODD, 2*NSIZE, "MO ")
+      SUBROUTINE FILLUYY(U, FLUX, LIMX)
+      IMPLICIT NONE
+      INTEGER LIMX, I
+      DOUBLE COMPLEX U(LIMX, LIMX)
+      DOUBLE PRECISION FLUX
+      DOUBLE COMPLEX ZI/(0.0, 1.0)/
+     
+      CALL SQUNITZ (U, ZI, LIMX)
 
       RETURN
       END
