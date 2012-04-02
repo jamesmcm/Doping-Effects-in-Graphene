d71525f9bef5556f5a034ff7a0c0ad562c0857b8
avshytov <avshytov@gmail.com>
2012-04-02 21:57:33 +0100

    Added CALCMULTXX, CALCMULTXY, FILLUXX, FILLUXY.

--- transfermatrix/tmatrixx.f
+++ transfermatrix/tmatrixx.f
@@ -19,15 +19,14 @@ C     NSIZE = LIMY/2
       CHARACTER GAUGE
 
       CALL SQUNIT (MULT, 2*NSIZE)
-      CALL FILLUYX(U, FLUX, NSIZE)
+      CALL FILLUXX(U, FLUX, NSIZE)
 
       CALL SQUNIT (T,      NSIZE)
       CALL SQUNIT (TTILDE, NSIZE)
       CALL SQZERO (R,      NSIZE)
       CALL SQZERO (RTILDE, NSIZE)
-
       DO I = 1, LIMX
-            CALL CALCMULTXY(E, FLUX, I, MULT, NSIZE)
+            CALL CALCMULTXX(E, FLUX, I, MULT, NSIZE)
             CALL GENABCD(MULT, U, A,B,C,D, NSIZE)
             CALL GENTANDRINC(A, B, C, D,
      +                       TINC, RINC, TTILDEINC, RTILDEINC,
@@ -38,7 +37,6 @@ C     NSIZE = LIMY/2
       END DO
       CALL SQSVDVALS(T, TVALS, NSIZE)
 
-c$$$  CheckUni2 is slightly faster --- AVS
       GETTRANSX = CHECKUNI2(T, R, TTILDE, RTILDE, NSIZE)
       RETURN
       END
@@ -88,5 +86,82 @@ c$$$      CALL INVERTMATRIX(N2, NSIZE)
       MULT(NSIZE+1:2*NSIZE, 1:NSIZE)=-E*N3
       MULT(NSIZE+1:2*NSIZE, NSIZE+1:2*NSIZE)=(E*E*N3) - N2
 
+      RETURN
+      END
+      
+      SUBROUTINE CALCMULTXX(E, FLUX, POS, MULT, NSIZE)
+C     NOTE LIMY MUST BE PASSED AS CONSTANT PARAMETER FOR THIS, LIMY MUST BE EVEN
+      IMPLICIT NONE
+      INTEGER LIMX, POS
+C     WRAPY ignored
+      INTEGER I/1/, NSIZE
+      DOUBLE PRECISION E, FLUX
+      DOUBLE COMPLEX CNUM
+
+      DOUBLE COMPLEX MULT(NSIZE*2, NSIZE*2), 
+     +     N3(NSIZE, NSIZE), N2(NSIZE, NSIZE)
+
+C     Odd is defined for odd leftmost column, even for even leftmost column
+      CALL SQZERO (N3, NSIZE)
+      CALL SQZERO (N2, NSIZE)
+
+      DO I = 1, NSIZE
+         N3(I, I)=1
+         N2(I, I)=1
+         IF (I .NE. 1) THEN
+            IF (MOD(POS,2) .EQ. 0) THEN
+               N2(I,I-1)=1
+            ELSE
+               N3(I,I-1)=1
+            ENDIF
+         ENDIF
+         IF (I .NE. NSIZE) THEN
+            IF (MOD(POS,2) .EQ. 0) THEN
+               N3(I,I+1)=1
+            ELSE
+               N2(I,I+1)=1
+            ENDIF
+         ENDIF
+      ENDDO
+
+C     M={{-N3^-1, E*N3^-1},{-E*N3^-1, E^2 N3^-1 - N2^-1}}
+
+      CALL INVERTMATRIX(N3, NSIZE)
+c$$$      CALL INVERTMATRIX(N2, NSIZE)
+
+      MULT(1:NSIZE, 1:NSIZE)=-1*N3
+      MULT(1:NSIZE, NSIZE+1:2*NSIZE)=E*N3
+      MULT(NSIZE+1:2*NSIZE, 1:NSIZE)=-E*N3
+      MULT(NSIZE+1:2*NSIZE, NSIZE+1:2*NSIZE)=(E*E*N3) - N2
+
+      RETURN
+      END
+
+      SUBROUTINE FILLUXY(U, FLUX, LIMX)
+      IMPLICIT NONE
+      INTEGER LIMX, I
+      DOUBLE COMPLEX U(LIMX, LIMX)
+      DOUBLE PRECISION FLUX
+      DOUBLE COMPLEX ZI/(0.0, 1.0)/
+     
+      CALL SQUNITZ (U, ZI, LIMX)
+
+      RETURN
+      END
+
+      SUBROUTINE FILLUXX(U, FLUX, LIMX)
+      IMPLICIT NONE
+      INTEGER LIMX, I
+      DOUBLE COMPLEX U(LIMX,LIMX)
+      DOUBLE PRECISION FLUX
+      DOUBLE COMPLEX   CNUM
+      DOUBLE COMPLEX ZI/(0.0, 1.0)/      
+
+      CALL SQZERO (U, LIMX)
+      DO I = 1, LIMX
+         CALL ZPOLAR(2 * FLUX*I, CNUM)
+         U(I,I)=ZI*CNUM
+      ENDDO
+     
       RETURN
       END
