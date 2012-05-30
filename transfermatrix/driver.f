a0700782e1f915cb9101282ca8013cb97dd92315
Will Smith <will_smith2@hotmail.co.uk>
2012-05-30 09:25:12 +0100

    Working code for average conductance with errorbars (using random TVALS for now)

--- transfermatrix/driver.f
+++ transfermatrix/driver.f
@@ -9,16 +9,20 @@
       DOUBLE PRECISION ZLANGE
       EXTERNAL CHECKUNI2
 
+      DOUBLE PRECISION ADDUPV
+      EXTERNAL ADDUPV
+
       
 C     For X current,  LIMY MUST be even, LIMX MUST BE >=3
 C     FOR Y current,  LIMX should be even if WRAPX = 1
       CHARACTER             CURRENT /'Y'/,
      +                      GAUGE   /'X'/
        
-      INTEGER, PARAMETER :: LIMX  = 3,
-     +                      LIMY  = 10,
+      INTEGER, PARAMETER :: LIMX  = 1000,
+     +                      LIMY  = 1000,
      +                      WRAPX = 0,
-     +                      WRAPY = 0
+     +                      WRAPY = 0,
+     +                      TIMES = 1000
       
       DOUBLE PRECISION      FLUX/0.0/
        
@@ -27,10 +31,15 @@ C     FOR Y current,  LIMX should be even if WRAPX = 1
       INTEGER, PARAMETER ::          NE   =  600
       
       INTEGER, PARAMETER :: MAXSIZE = 10000
-      DOUBLE  PRECISION TVALS(MAXSIZE)
+      DOUBLE PRECISION TVALS(MAXSIZE)
       INTEGER NTVALS
       DOUBLE PRECISION E, CONDA/-1.0/, G
       INTEGER IE/0/
+
+      DOUBLE PRECISION V(LIMX,LIMY), TOTALV(TIMES), MEANV, DEVV
+      DOUBLE PRECISION, PARAMETER :: U    = 1.0,
+     +                               ALAT = 0.5,
+     +                               BLAT = 0.5
     
 
 C$$$  READS COMMAND LINE ARGUMENT AS LIMY
@@ -38,16 +47,25 @@ c      CALL GETARG(1, VALUE)
 c      READ(UNIT=VALUE, FMT=*) LIMY
 c$$$      CALL CALCMULTXY(LIMY/2, MODD, MEVEN, E, FLUX)
 
+c$$$  Generates the matrix of vacancy locations
+      DO IE = 1, TIMES
+         CALL GENVAC(V, LIMX, LIMY, U, ALAT, BLAT)
+         TOTALV(IE) = ADDUPV(V, LIMX, LIMY)
+      END DO
+      CALL MEANDEVV(TOTALV, TIMES, MEANV, DEVV)      
 
-      DO IE = 0, NE + 1
-         E = EMIN + ( (EMAX - EMIN) * IE) / NE
-         CONDA = GETTRANS(CURRENT, GAUGE,
-     +                   TVALS, NTVALS,
-     +                   LIMX, LIMY,
-     +                   E,    FLUX,
-     +                   WRAPX)
-         G    = CONDUCTANCE (TVALS, NTVALS)
-         WRITE(*,50) E, G, CONDA
+      WRITE (*,*) LIMX * LIMY, ',', ALAT, BLAT, ',', MEANV, '+/-', DEVV
+c      WRITE (*,*) (TOTALV(IE), IE = 1, TIMES)
+
+c      DO IE = 0, NE + 1
+c         E = EMIN + ( (EMAX - EMIN) * IE) / NE
+c         CONDA = GETTRANS(CURRENT, GAUGE,
+c     +                   TVALS, NTVALS,
+c     +                   LIMX, LIMY,
+c     +                   E,    FLUX,
+c     +                   WRAPX)
+c         G    = CONDUCTANCE (TVALS, NTVALS)
+c         WRITE(*,50) E, G, CONDA
 
 c$$$     This is gfortran function to flush the output
 c$$$     so that the data are written to the file immediately
@@ -55,8 +73,9 @@ c$$$     If you are not using gfortran, and cannot compile this,
 c$$$     comment it out -- AVS
          CALL FLUSH()
 
-      END DO
+c      END DO
 
  50   FORMAT (F15.5,20ES20.5E3)
       STOP
       END
+
