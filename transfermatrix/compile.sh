55fc2436a906dd90412e13579a3ba21749f68a56
Chris <cbeckerleg101@gmail.com>
2012-06-13 13:21:12 +0100

    Updated to use "driver_will.f"

--- transfermatrix/compile.sh
+++ transfermatrix/compile.sh
@@ -1,2 +1,2 @@
-gfortran driver_will_temp.f linalg.f tmatrix.f tmatrixx.f tmatrixy.f util.f trupdate.f unitarity.f potential.f -o tmsim -L/usr/local/lib -lm -lblas -llapack
+gfortran driver_will.f linalg.f tmatrix.f tmatrixx.f tmatrixy.f util.f trupdate.f unitarity.f potential.f -o tmsim -L/usr/local/lib -lm -lblas -llapack
 
